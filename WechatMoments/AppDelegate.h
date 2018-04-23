796aa6c40b9001a06137af0914befbb9117c439c
liuhuagong <liuhg@ifenglian.com>
2018-04-23 17:35:18 +0800

    创建工程

---
+++ WechatMoments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WechatMoments
+//
+//  Created by lmy on 2018/4/23.
+//  Copyright © 2018年 lmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WechatMoments/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WechatMoments
+//
+//  Created by lmy on 2018/4/23.
+//  Copyright © 2018年 lmy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
